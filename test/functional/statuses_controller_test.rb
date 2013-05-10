698f4c73620df4ed079b24ced69541f4c61f4f6e
Ryan Flournoy <ryanflournoy@yahoo.com>
2013-05-10 11:44:07 -0700

    dkfajodf

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -38,6 +38,16 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should create status for the current user when logged in" do
+    sign_in users(:ryan)
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:jim).id }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:ryan).id
+  end
+
   test "should show status" do
     get :show, id: @status
     assert_response :success
@@ -48,27 +58,24 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should update status" do
-    put :update, id: @status, status: { content: @status.content}
+  
+
+
+
+  
+
+  
+  test "should update status for the current user when logged in" do
+    sign_in users(:ryan)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:jim).id}
     assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:ryan).id
   end
 
   test "should destroy status" do
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
     end
-
-  test "should redirect status update when not logged in" do
-    put :update, id: @status, status: { content: @status.content }
-    assert_response :redirect
-    assert_redirected_to new_user_session_path
-  end
-
-  test "should update status when logged in" do
-    sign_in users(:jason)
-    put :update, id: @status, status: { content: @status.content }
-    assert_redirected_to status_path(assigns(:status))
-  end
 
 
 
